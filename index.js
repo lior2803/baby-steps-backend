060429ad97afc54b9422962574a0967210ca7d24
Lior <liorbu@il.ibm.com>
2017-12-31 21:24:34 +0200

    fix in index.js

--- index.js
+++ index.js
@@ -46,9 +46,8 @@ app.get('/friends', function(req, res) {
 
 var server = app.listen(3001, function() {
 
-  var host = server.address().address;
   var port = server.address().port;
 
-  console.log("Example app listening at http://%s:%s", host, port);
+  console.log("Example app listening at http://%s:%s/friends", "localhost", port);
 
 });
